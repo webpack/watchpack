570c75ef6a94ed4a042425d80621384745d755b2
shellscape <andrew@shellscape.org>
2017-12-09 22:28:40 -0500

    working on tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,6 @@
 'use strict';
 
-// const assert = require('assert');
-//
-// describe('it', () => {
-//   it('should', () => {
-//     assert(true, true);
-//   });
-// });
-
-require('./tests/assumptions');
+// require('./tests/assumptions');
 // require('./tests/case-sensitivity');
 // require('./tests/directory-watcher');
-// require('./tests/watchpack');
+require('./tests/watchpack');
