dc01813bd30ff84212551a4172a768752a9e51bf
Tobias Koppers <tobias.koppers@googlemail.com>
2020-08-27 21:13:23 +0200

    add time logging to test, make test faster, increase timeout

--- test/ManyWatchers.js
+++ test/ManyWatchers.js
@@ -11,11 +11,14 @@ const fixtures = path.join(__dirname, "fixtures");
 const testHelper = new TestHelper(fixtures);
 
 describe("ManyWatchers", function() {
-	this.timeout(240000);
+	this.timeout(600000);
 	beforeEach(testHelper.before);
 	afterEach(testHelper.after);
 
 	it("should watch more than 4096 directories", done => {
+		console.time("creating files");
+		// windows is very slow in creating so many files
+		// this can take about 1 minute
 		const files = [];
 		for (let i = 1; i < 5000; i++) {
 			let highBit = 1;
@@ -40,24 +43,31 @@ describe("ManyWatchers", function() {
 		}
 		testHelper.file("file");
 		files.push(path.join(fixtures, "file"));
+		console.timeEnd("creating files");
 		testHelper.tick(10000, () => {
 			const w = new Watchpack({
 				aggregateTimeout: 1000
 			});
 			w.on("aggregated", function(changes) {
+				console.timeEnd("detecting change event");
 				Array.from(changes).should.be.eql([
 					path.join(fixtures, "4096/900/file")
 				]);
 				w.close();
 				done();
 			});
-			for (let i = 100; i < files.length; i += 987) {
+			console.time("creating/closing watchers");
+			// MacOS is very slow in creating and destroying watchers
+			// This can take about 2 minutes
+			for (let i = 100; i < files.length; i += 2432) {
 				for (let j = 0; j < files.length - i; j += 987) {
 					w.watch({ files: files.slice(j, j + i) });
 				}
 			}
 			w.watch({ files });
+			console.timeEnd("creating/closing watchers");
 			testHelper.tick(10000, () => {
+				console.time("detecting change event");
 				testHelper.file("4096/900/file");
 			});
 		});
